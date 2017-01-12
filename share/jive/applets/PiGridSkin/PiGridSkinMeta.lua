07725d93946e57ed1dc08883276e6e5ecec03200
Michael Herger <mherger@logitech.com>
2017-01-12 12:01:34 +0100

    Clone the Joggler skin to start work on a grid based version to make better use of the original Raspberry Pi 7“ display

---
+++ share/jive/applets/PiGridSkin/PiGridSkinMeta.lua
@@ -0,0 +1,54 @@
+
+--[[
+=head1 NAME
+
+applets.PiGridSkin.WQVGAsmallSkinMeta 
+
+=head1 DESCRIPTION
+
+See L<applets.WQVGAsmallSkin.WQVGAsmallSkinApplet>.
+
+=head1 FUNCTIONS
+
+See L<jive.AppletMeta> for a description of standard applet meta functions.
+
+=cut
+--]]
+
+
+local oo            = require("loop.simple")
+
+local AppletMeta    = require("jive.AppletMeta")
+
+local appletManager = appletManager
+local jiveMain      = jiveMain
+
+
+module(...)
+oo.class(_M, AppletMeta)
+
+
+function jiveVersion(self)
+	return 1, 1
+end
+
+function defaultSettings(self)
+	return {}
+end
+
+function registerApplet(self)
+	jiveMain:registerSkin(self:string("PIGRID_SKIN"), "PiGridSkin", "skin")
+end
+
+
+--[[
+
+=head1 LICENSE
+
+Copyright 2010 Logitech. All Rights Reserved.
+
+This file is licensed under BSD. Please see the LICENSE file for details.
+
+=cut
+--]]
+
